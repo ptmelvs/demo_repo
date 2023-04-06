8badf88d27c00a1a50ac9ec2781b0acbd7965411
Nikita Kapusta <zgadena2@gmail.com>
2023-04-06 15:47:36 +0600

    Task 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('AAA')
+# Task 1
+
+sentence = input('Введите предложение: ')
+chars_popularity = {}
+words_popularity = {}
+
+for symbol in sentence:
+    chars_popularity[symbol] = sentence.count(symbol)
+
+for word in sentence.split():
+    words_popularity[word] = sentence.count(word)
+
+print(words_popularity)
+print(chars_popularity)
+
+# Task 2
